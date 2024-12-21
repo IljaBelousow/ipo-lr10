77d4440f52d8723dd293c12f5272c0dd3f04023d
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-21 12:29:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,41 +7,59 @@ page = requests.get(url)
 file = 'data.json'
 index = 'index.html'
 
-allQuote = []
-allAuthor = []
+allquote = []
+allauthor = []
 saved_file = []
 
 soup = BeautifulSoup(page.text, 'html.parser')
-Quote = soup.findAll('span', class_='text')
-Author = soup.findAll('small', class_='author')
+quote = soup.findAll('span', class_='text')
+author = soup.findAll('small', class_='author')
 
-for i in Quote:
-    allQuote.append(i.text)
-for i in Author:
-    allAuthor.append(i.text)
+for i in quote:
+    allquote.append(i.text)
+for i in author:
+    allauthor.append(i.text)
 
-for i in range(len(allQuote)):
-    print(f'{i + 1}. Quote: {allQuote[i]}; Author: {allAuthor[i]};')
+for i in range(len(allquote)):
+    print(f'{i + 1}. quote: {allquote[i]}; author: {allauthor[i]};')
 
 with open(file, 'w+', encoding='utf-8') as f:
-    for i in range(len(allQuote)):
-        saved = {'Quote': allQuote[i], 'Author': allAuthor[i]
-                     }
+    for i in range(len(allquote)):
+        saved = {'quote': allquote[i], 'author': allauthor[i]}
         saved_file.append(saved)
-    json.dump(saved_file, f, indent = 4, ensure_ascii = False)
+    json.dump(saved_file, f, indent=4, ensure_ascii=False)
 
-with open(index, "w+" , encoding='utf-8') as f: 
-    f.write("<html><head><title>Quotes</title></head><body>\n")  
-    f.write('<h1><p align="center" > <a href="https://quotes.toscrape.com/">ссылка</h1></a></p>\n') 
-    f.write('<body bgcolor="#063645">\n') 
-    f.write('<table cellspacing="3"  bordercolor="blue"  BGCOLOR= #c9700a border="1" align="center"') 
-    f.write("<table>\n") 
-    f.write("<tr>\n")
-    f.write(" <td>Цитата</td>\n<td>Автор</td>\n<td>Номер</td>\n</tr>\n")
-    
-    with open(file, 'r', encoding='utf-8') as file_open:
-        file_saved = json.load(file_open)
-        for i in range(len(file_saved)):
-            f.write(f"<tr>\n<th>{file_saved[i]['Quote']}</th>\n<th>{file_saved[i]['Author']}</th>\n<th>{i+1}</th>\n")
-    f.write("</table>\n") 
-    f.write("</body></html>")
+html_content = """
+<html>
+<head>
+    <title>quotes</title>
+</head>
+<body bgcolor="#063645">
+    <h1><p align="center"><a href="https://quotes.toscrape.com/">ссылка</a></p></h1>
+    <table cellspacing="3" bordercolor="blue" bgcolor="#c9700a" border="1" align="center">
+        <tr>
+            <td>Цитата</td>
+            <td>Автор</td>
+            <td>Номер</td>
+        </tr>
+"""
+
+with open(file, 'r', encoding='utf-8') as file_open:
+    file_saved = json.load(file_open)
+    for i in range(len(file_saved)):
+        html_content += f"""
+        <tr>
+            <th>{file_saved[i]['quote']}</th>
+            <th>{file_saved[i]['author']}</th>
+            <th>{i + 1}</th>
+        </tr>
+        """
+
+html_content += """
+    </table>
+</body>
+</html>
+"""
+
+with open(index, "w+", encoding='utf-8') as f:
+    f.write(html_content)
